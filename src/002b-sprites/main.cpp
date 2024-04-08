a85b9bff839f27e5b2f31216e56e7c7c15af5db8
Peter Harrison <peter.harrison@helicron.net>
2024-04-08 10:20:24 +0100

    update 002b

--- src/002b-sprites/main.cpp
+++ src/002b-sprites/main.cpp
@@ -13,8 +13,8 @@ int main() {
   sf::ContextSettings settings;
   settings.antialiasingLevel = 8;  // the number of multisamplings to use. 4 is probably fine
   sf::RenderWindow window{sf::VideoMode(600, 400), "002-shapes-and-textures", sf::Style::Default, settings};
-
   window.setVerticalSyncEnabled(true);
+  sf::FloatRect visibleArea(0, 0, (float)window.getSize().x, (float)window.getSize().y);
 
   /***
    * Textures are graphical elements that get loaded into the graphic card memory. Once there, they can
@@ -28,28 +28,22 @@ int main() {
    * texture from that image.
    */
   /// First let us load an image:
-  sf::Image space_things;
+  sf::Texture space_things;
   if (!space_things.loadFromFile("./assets/images/spaceship.png")) {
-    std::cerr << "Unable to load spaceship images\n";
+    std::cerr << "Unable to load spaceship textures\n";
     exit(1);
   }
-  /// Then use it to create the texture on the GPU
-  sf::Texture texture;
-  texture.loadFromImage(space_things);
 
   /// Rectangles are shapes with a width and height
   /// They can have colours and textures as well as outlines
-  sf::Vector2f size(55, 53);
-  sf::RectangleShape rect(size);
-  rect.setOrigin(size.x / 2, size.y / 2);
-  rect.setPosition(300, 200);
-  /// and we can add a texture reference to the rectangle
-  /// By default the entire texture will be scaled to fit the rectangle
-  rect.setTexture(&texture);
-  /// So we map only a portion of the texture to pull out one image
-  rect.setTextureRect(sf::IntRect(39, 0, 40, 36));
-  rect.setTextureRect(sf::IntRect(0, 153, 55, 53));
-  rect.scale(2, 3);
+  sf::Sprite ship(space_things);
+  sf::Rect rect_left(0, 40, 48, 36);
+  sf::Rect rect_main(48, 0, 48, 36);
+  sf::Rect rect_right(96, 40, 48, 36);
+  ship.setTextureRect(rect_main);
+  ship.setOrigin(rect_main.width / 2.0f, rect_main.height / 2.0f);
+  ship.setPosition(visibleArea.width / 2, visibleArea.height / 2);
+  ship.scale(1, 1);
 
   /// Sprites are simpler and _require_ a texture. They are the drawable
   /// representation of a texture
@@ -60,19 +54,11 @@ int main() {
     exit(1);
   }
 
-  /// The chicken will be animated by switching the texture rectangle in
-  /// a predefined sequence
-  const int seq[] = {1, 2, 1, 0};
-  sf::Sprite chick;
-  chick.setTexture(chicken);
-  chick.setTextureRect(sf::IntRect(0, 0, 32, 32));
-  chick.setOrigin(16, 16);
-  chick.setPosition(180, 200);
-  chick.scale(2, 2);
-  chick.setColor(sf::Color(255, 255, 0, 255));
-
   sf::Clock deltaClock{};
   int state = 0;
+  bool key_down = false;
+  float vx = 0;
+  float vmax = 150.0;  // pixels per second
 
   /// this is the 'game loop'
   while (window.isOpen()) {
@@ -87,33 +73,42 @@ int main() {
       /// This works because the scene is effectively infinite and the window is just
       /// a view of it starting at (0,0)
       if (event.type == sf::Event::Resized) {
-        sf::FloatRect visibleArea(0, 0, (float)event.size.width, (float)event.size.height);
+        visibleArea.width = (float)event.size.width;
+        visibleArea.height = (float)event.size.height;
         /// Note that constructing a view with a sf::Rect defines the corners
         /// Using two points defines the _centre_ and the width,height
-        window.setView(sf::View(visibleArea));  // or everything distorts
+        window.setView(sf::View(visibleArea));  /// or everything distorts
+        ship.setPosition(visibleArea.getSize().x / 2, visibleArea.getSize().y / 2);
       }
     }
 
-    /// update the objects
-    sf::Time time = deltaClock.restart();
-    chick.rotate(90 * time.asSeconds());  /// 90 degrees per second
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Left)) {
+      ship.setTextureRect(rect_left);
+      ship.setOrigin(rect_left.width / 2.0f, rect_left.height / 2.0f);
+      vx = -vmax;
+    } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Right)) {
+      ship.setTextureRect(rect_right);
+      ship.setOrigin(rect_right.width / 2.0f, rect_right.height / 2.0f);
+      vx = vmax;
+    } else {
+      ship.setTextureRect(rect_main);
+      ship.setOrigin(rect_main.width / 2.0f, rect_main.height / 2.0f);
+      vx = 0;
+    }
 
-    float angle = chick.getRotation();
-    float dx = std::cos((angle - 90) * 3.14 / 180) * 200;
-    float dy = std::sin((angle - 90) * 3.14 / 180) * 200;
-    chick.move(sf::Vector2f(dx, dy) * time.asSeconds());
-    /// calculate the next animation frame location
-    chick.setTextureRect(sf::IntRect(32 * seq[state / 5], 0, 32, 32));
-    state = (++state) % 20;
+    /// update the objects
+    sf::Time dt = deltaClock.restart();
 
     /// rotate the rect and its texture rotates with it.
     /// the graphics card handles all that.
-    rect.rotate(45 * time.asSeconds());
-
+    //    ship.rotate(45 * time.asSeconds());
+    float deltax = vx * dt.asSeconds();
+    if ((ship.getPosition().x + deltax > 24) && ((ship.getPosition().x + deltax) < (visibleArea.width - 24))) {
+      ship.move(deltax, 0);
+    }
     /// and redraw the window
     window.clear();
-    window.draw(rect);
-    window.draw(chick);
+    window.draw(ship);
     window.display();
   }
 
