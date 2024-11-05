912eeee3682fff8cc5ec4312f3290e1842aedfda
Peter Harrison <peter.harrison@helicron.net>
2024-11-05 14:40:52 +0000

    add notes to 001

--- src/001-basic-window-and-shapes/main.cpp
+++ src/001-basic-window-and-shapes/main.cpp
@@ -6,6 +6,8 @@
 
 /***
  * Create a simple SFML window with custom title text and a couple of shapes drawn in it
+ *
+ * There is a lot in here but it just covers the essentials of drawing stuff to the window.
  */
 
 int main() {
@@ -24,7 +26,9 @@ int main() {
   /// Video Modes. If using fullscreen and the requested video mode is one that you monitor can be set to
   /// then it will be and, after the program exits, it will stay that way!
   sf::RenderWindow window{sf::VideoMode(1920, 1080), WINDOW_TITLE, sf::Style::Default, settings};
+
   window.setFramerateLimit(60);
+  window.setVerticalSyncEnabled(true);
   /// You can set the window to a position in the monitor reference frame but this is likely not what you think
   /// The monitor reference frame is a rectangle enclosing all the monitor screens.
   //  window.setPosition(sf::Vector2i(20, 20));
@@ -39,32 +43,44 @@ int main() {
   circle.setPosition(100, 100);
 
   /// Convex shapes are defined as a list of points that are rendered as triangles
-  /// the order of the points is important and the shape must be convex if you are to avoid strangeness
+  /// the order of the points is important as the polygon is rendered from triplets
+  /// taken in order from the list.
+  /// The the shape must be convex if you are to avoid strangeness
   /// create an empty shape
   sf::ConvexShape convex;
   /// resize it to 5 points
   convex.setPointCount(5);
 
   /// define the points
-  convex.setPoint(0, sf::Vector2f(0, 0));
+  convex.setPoint(0, sf::Vector2f(40, 0));
   convex.setPoint(1, sf::Vector2f(150, 10));
   convex.setPoint(2, sf::Vector2f(120, 90));
   convex.setPoint(3, sf::Vector2f(30, 100));
   convex.setPoint(4, sf::Vector2f(0, 50));
+
   /// You can fill all the triangles with the same colour together
   convex.setFillColor(sf::Color::Cyan);
-  convex.setOrigin(convex.getGlobalBounds().width / 2, convex.getGlobalBounds().height / 2);
+
+  /// The origin is the point used whenever we position the object in the window
+  /// it will also be the point about which rotations are centered
+  /// the default origin is the top left corner of the bounding box
+  /// It can be set to any location
+  //  convex.setOrigin(convex.getGlobalBounds().width / 2, convex.getGlobalBounds().height / 2);
+  //  convex.setOrigin(convex.getPoint(3));
   convex.setPosition(300, 100);
 
   /// Rectangles are simple shapes with a width and height
   /// Internally they are just two triangles
+  /// By default, the origin is the top left
   sf::RectangleShape rect(sf::Vector2f(80, 20));
+  /// Colours have an alpha channel. The fill here will be translucent at 50%
   rect.setFillColor(sf::Color(255, 255, 0, 128));
   rect.setOutlineColor(sf::Color::Red);
   rect.setOutlineThickness(2.f);
   rect.setPosition(60, 110);
 
-  /// There is no class for a line as such. Instead, for a line with finite thickness, use a rectangle
+  /// There is no class for a line as such. There are two options:
+  /// For a line with finite thickness, use a rectangle
   sf::RectangleShape thick_line(sf::Vector2f(150, 5));
   thick_line.setFillColor(sf::Color(255, 0, 255, 128));
   thick_line.setOrigin(thick_line.getSize() / 2.f);
@@ -91,11 +107,14 @@ int main() {
   vertices[5].color = (sf::Color::Green);
   vertices[6].color = (sf::Color::Red);
 
+  /// These are the ways you can use a vertex list to draw shapes
+  /// Triangles have their vertex colours interpolated across the triangle
   sf::Vertex lines[7];
   sf::Vertex line_strip[7];
   sf::Vertex triangles[7];
   sf::Vertex triangle_strip[7];
   sf::Vertex triangle_fan[7];
+
   for (int i = 0; i < 7; i++) {
     lines[i] = vertices[i];
     line_strip[i] = vertices[i];
@@ -112,9 +131,11 @@ int main() {
   while (window.isOpen()) {
     sf::Event event{};
     while (window.pollEvent(event)) {
+      /// This is how you listen for the window close button being pressed
       if (event.type == sf::Event::Closed) {
         window.close();
       }
+      /// this is how you listen for a specific key being pressed
       if (event.type == sf::Event::KeyPressed) {
         if (event.key.scancode == sf::Keyboard::Scancode::Escape) {
           window.close();
@@ -129,9 +150,16 @@ int main() {
         window.setView(sf::View(visibleArea));  // or everything distorts
       }
     }
+    /// When a Clock is restarted, the current elapsed time is returned.
+    /// restart the clock once per frame to get the frame refresh interval
     sf::Time time = deltaClock.restart();
-    thick_line.rotate(36 * time.asSeconds());
-    convex.rotate(90 * time.asSeconds());
+    thick_line.rotate(36 * time.asSeconds());  // 36 degrees per second
+    convex.rotate(90 * time.asSeconds());      // 90 degrees per second
+
+    /// Only after updating everything should you redraw the window.
+    /// Just do the drawing in this section.
+    /// Drawing is done to an off screen buffer in the GPU.
+    /// always start from a clean slate
     window.clear();
     window.draw(circle);
     window.draw(rect);
@@ -143,6 +171,8 @@ int main() {
     window.draw(triangles, 6, sf::Triangles);
     window.draw(triangle_strip, 6, sf::TriangleStrip);
     window.draw(triangle_fan, 6, sf::TriangleFan);
+    /// This makes the off-screen buffer live and will wait for the refresh event
+    /// specified above. Normally a simple wait fo elapsed time is used
     window.display();
   }
 
