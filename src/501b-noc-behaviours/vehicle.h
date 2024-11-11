f1bfd2ea8ba7870ff406f81bd73f5dcec4bc5878
Peter Harrison <peter.harrison@helicron.net>
2024-11-11 14:05:43 +0000

    add definitions for M_PI

--- src/501b-noc-behaviours/vehicle.h
+++ src/501b-noc-behaviours/vehicle.h
@@ -7,6 +7,9 @@
 
 #include <random>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 float map(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
