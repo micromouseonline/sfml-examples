b0bc58aa2873b7c7a29f01b16256552faf15f419
Peter Harrison <peter.harrison@helicron.net>
2024-11-22 23:09:53 +0000

    add a font and a vector drawing method

--- libs/utils/drawing.h
+++ libs/utils/drawing.h
@@ -7,11 +7,20 @@
 
 #include <SFML/Graphics.hpp>
 
-inline void draw_line(sf::RenderTarget& target, sf::Vector2f pos, sf::Vector2f vec, sf::Color color = sf::Color::White) {
+inline void draw_line(sf::RenderTarget& target, sf::Vector2f start, sf::Vector2f end, sf::Color color = sf::Color::White) {
   sf::Vertex line[2];
-  line[0].position = {pos.x, pos.y};
+  line[0].position = start;
   line[0].color = color;
-  line[1].position = {vec.x, vec.y};
+  line[1].position = end;
+  line[1].color = color;
+  target.draw(line, 2, sf::Lines);
+}
+
+inline void draw_vector(sf::RenderTarget& target, sf::Vector2f pos, sf::Vector2f vec, sf::Color color = sf::Color::White) {
+  sf::Vertex line[2];
+  line[0].position = pos;
+  line[0].color = color;
+  line[1].position = pos + vec;
   line[1].color = color;
   target.draw(line, 2, sf::Lines);
 }
