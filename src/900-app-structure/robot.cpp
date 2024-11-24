7d734cfde9e3b456b1f56c770843ab7e65bd6964
Peter Harrison <peter.harrison@helicron.net>
2024-11-24 16:03:15 +0000

    The robot has a more robust way to simulate the ISR

--- src/900-app-structure/robot.cpp
+++ src/900-app-structure/robot.cpp
@@ -4,27 +4,39 @@
 
 #include "robot.h"
 #include <iostream>
+#include <mutex>
 #include <thread>
 #include "application.h"
 
-Robot::Robot() : m_running(false), m_pose(300.0f, 300.0f), m_orientation(0.0f) {
+Robot::Robot() : m_running(false), m_pose(1300.0f, 1300.0f), m_orientation(0.0f) {
 }
 
 Robot::~Robot() {
   Stop();
 }
 
+/***
+ * The main application should call the Start() method to start the robot, passing
+ * in a pointer to itself so that callbacks can be used. Fo example, the robot
+ * can request sensor data from the application.
+ *
+ * Once started, the robot will run until Stop() is called.
+ *
+ * This robot instance will also start its own thread that repeatedly calls the
+ * systick() method. In this case, systick runs at about 1kHz. Actually a little
+ * less because we run the code then sleep for 1ms. Although the timing will not
+ * be completely accurate, it should suffice for simulation.
+ *
+ * We could
+ *
+ * @param app - the calling thread.
+ */
 void Robot::Start(Application* app) {
   if (!m_running) {
     m_application = app;
     m_running = true;
     m_thread = std::thread(&Robot::Run, this);
-    m_systick_thread = std::thread([this]() {
-      while (m_running) {
-        systick();
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
-      }
-    });
+    m_systick_thread = std::thread(&Robot::systick, this);
   }
 }
 
@@ -40,54 +52,80 @@ void Robot::Stop() {
   }
 }
 
+/***
+ * The systick() method runs in its own thread, with the loop running at, in
+ * this case, about 1kHz. The systick() method simulates an interrupt triggered
+ * by a timer on the hardware.
+ *
+ * In the real hardware, operations during the Interrupt Service Routine (ISR) are
+ * mutually exclusive with the main code. This would not normally be the case here,
+ * so everything that happens in systick() is guarded with a mutex. This ensures
+ * the code in systick() can run and modify state as needed, while the top-level
+ * robot code will block as soon as it tries to access any of the resources guarded
+ * by the mutex, thus simulating the behavior of the hardware interrupt.
+ *
+ * If the top-level code needs to guarantee that systick() will not change any
+ * shared state, it can also lock the same mutex for the duration of a critical
+ * section. If the mutex is already locked, the top-level code will block until
+ * systick() has finished that iteration. Then the top-level code will acquire the
+ * mutex and block systick() until it is released.
+ *
+ * Care must be taken to ensure that these mutually cooperative functions do
+ * not end up in a deadlock.
+ */
+
 void Robot::systick() {
-  ticks++;
-  ReadSensorValues();
+  while (m_running) {
+    // The mutex will lock out the main thread while this block runs.
+    CRITICAL_SECTION(m_SystickMutex) {
+      ticks++;
+      // MonitorEvents();
+      ReadSensorValues();
+      // UpdateMotion();
+      m_orientation += m_AngularVelocity * m_LoopTime;
+      if (m_orientation >= 360.0f) {
+        m_orientation -= 360.0f;
+      } else if (m_orientation < 0.0f) {
+        m_orientation += 360.0f;
+      }
+      // Update position based on linear velocity and orientation
+      float orientationRad = m_orientation * 3.14159f / 180.0f;
+      m_pose.x += m_LinearVelocity * std::cos(orientationRad) * m_LoopTime;
+      m_pose.y += m_LinearVelocity * std::sin(orientationRad) * m_LoopTime;
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+  }
 }
 
+/***
+ * The run() method is what gets called from the thread initialisation
+ * It should run continuously when started. An atomic flag, m_running,
+ * is used so that the parent thread can signal an orderly termination.
+ */
 void Robot::Run() {
   /// TODO: for testing, just have the robot run around in a circle.
-  const sf::Vector2f initialCenter(300.0f, 300.0f);  // Center of the circle
-  const float initialRadius = 100.0f;                // Radius of the circle
-  const float initialAngularSpeed = 60.0f;           // Degrees per second
+  const sf::Vector2f initialCenter(2200.0f, 1200.0f);  // Center of the circle
+  const float initialRadius = 400.0f;                  // Radius of the circle
+  const float initialAngularSpeed = 120.0f;            // Degrees per second
 
   m_control.SetCircularTrajectory(initialCenter, initialRadius, initialAngularSpeed);
 
   sf::Clock clock;
   while (m_running) {
     sf::Time elapsed = clock.restart();  // Time since last update
-    float deltaTime = elapsed.asSeconds();
-    deltaTime = 0.01f;
+                                         //    float deltaTime = elapsed.asSeconds();
+                                         //    deltaTime = 0.01f;
 
     // Get the velocities from the RobotControl class
-    float linearVelocity = m_control.GetLinearVelocity();
-    float angularVelocity = m_control.GetAngularVelocity();
-
-    {
-      std::lock_guard<std::mutex> lock(m_stateMutex);
-
-      // Update orientation
-      m_orientation += angularVelocity * deltaTime;
-
-      // Keep orientation within [0, 360) degrees
-      if (m_orientation >= 360.0f) {
-        m_orientation -= 360.0f;
-      } else if (m_orientation < 0.0f) {
-        m_orientation += 360.0f;
-      }
-
-      // Update position based on linear velocity and orientation
-      float orientationRad = m_orientation * 3.14159f / 180.0f;  // Convert to radians for trigonometric functions
-      m_pose.x += linearVelocity * std::cos(orientationRad) * deltaTime;
-      m_pose.y += linearVelocity * std::sin(orientationRad) * deltaTime;
-    }
+    m_LinearVelocity = m_control.GetLinearVelocity();
+    m_AngularVelocity = m_control.GetAngularVelocity();
 
     std::this_thread::sleep_for(std::chrono::milliseconds(10));  // 100 Hz loop
   }
 }
 
 void Robot::UpdateState() {
-  std::lock_guard<std::mutex> lock(m_stateMutex);
+  std::lock_guard<std::mutex> lock(m_SystickMutex);
   // Update the robot's position and orientation (simulation logic here)
 }
 
@@ -97,12 +135,12 @@ void Robot::ProcessControl() {
 }
 
 sf::Vector2f Robot::GetPose() const {
-  std::lock_guard<std::mutex> lock(m_stateMutex);
+  std::lock_guard<std::mutex> lock(m_SystickMutex);
   return m_pose;
 }
 
 float Robot::GetOrientation() const {
-  std::lock_guard<std::mutex> lock(m_stateMutex);
+  std::lock_guard<std::mutex> lock(m_SystickMutex);
   return m_orientation;
 }
 
@@ -113,12 +151,12 @@ void Robot::ReadSensorValues() {
 }
 
 SensorValues& Robot::GetSensorValues() {
-  std::lock_guard<std::mutex> lock(m_stateMutex);
+  std::lock_guard<std::mutex> lock(m_SystickMutex);
   return m_sensorValues;
 }
 
 uint32_t Robot::millis() {
-  std::lock_guard<std::mutex> lock(m_stateMutex);
+  std::lock_guard<std::mutex> lock(m_SystickMutex);
   return ticks;
   ;
 }
--- src/900-app-structure/robot.h
+++ src/900-app-structure/robot.h
@@ -6,12 +6,43 @@
 #define ROBOT_H
 
 #include <SFML/Graphics.hpp>
+#include <mutex>
 #include <thread>
 #include "collision-object.h"
 #include "robot-control.h"
 #include "robot-model.h"
 #include "sensor-data.h"
 #include "sensor.h"
+
+/***
+ * TODO Not sure this is the best place for this macro
+ *
+ * the macro looks odd because it uses a C++17 feature where
+ * the if statement condition can include an initialiser.
+ * This feature allows you to initialize variables within the
+ * if statement, followed by a condition that determines if
+ * the block should be executed. It's a neat way to declare
+ * and initialise variables that are only used within the
+ * scope of the if statement. Use it like this:
+ *
+ * if (initialiser; condition){
+ *   // code that will run if condition is true
+ * }
+ *
+ * The scope of any variables declared in the initializer
+ * of an if statement is limited to the entire if block,
+ * including both the condition and the code within the
+ * braces {} of the if statement.
+ *
+ * Use the macro like this
+ *
+ * CRITICAL_SECTION(m_systick_mutex){
+ *   // guarded code
+ * }
+ *
+ */
+
+#define CRITICAL_SECTION(the_mutex) if (std::lock_guard<std::mutex> lock(the_mutex); true)
 
 /// forward declarations
 class Application;
@@ -63,10 +94,13 @@ class Robot {
   std::thread m_systick_thread;
   std::thread m_thread;
   std::atomic<bool> m_running;  // Thread control flag
-  sf::Vector2f m_pose;          // (x, y) position
-  float m_orientation;          // Orientation in radians
+  float m_AngularVelocity = 0.0f;
+  float m_LinearVelocity = 0.0f;
+  float m_LoopTime = 0.001f;
+  sf::Vector2f m_pose;  // (x, y) position
+  float m_orientation;  // Orientation in radians
 
-  mutable std::mutex m_stateMutex;       // Protects access to m_pose and m_orientation
+  mutable std::mutex m_SystickMutex;     // Protects access to m_pose and m_orientation
   SensorValues m_sensorValues;           // Current sensor readings
   RobotControl m_control;                // Higher-level control logic
   Application *m_application = nullptr;  // Pointer to the Application (for querying sensor readings)
